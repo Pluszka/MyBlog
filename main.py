a5545771f7da36a6c5868ae473001f8072eadcbf
Pliszka <karoliniakkjk@gmail.com>
2022-04-06 00:34:16 +0200

    home page desc of articles

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from post import Post
 
-
+database = Post()
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", articles=database.get_blog() )
 
 if __name__ == "__main__":
     app.run(debug=True)
