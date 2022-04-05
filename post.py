9d0007267976cbff5b767ae017ea5af147622c29
Pliszka <karoliniakkjk@gmail.com>
2022-04-05 23:36:31 +0200

    class Post created

--- post.py
+++ post.py
@@ -1,2 +1,11 @@
+import requests
+
+
 class Post:
-    pass
+    def __init__(self):
+        self.API_BLOG = 'https://api.npoint.io/c790b4d5cab58020d391'
+
+    def get_blog(self):
+        response = requests.get(self.API_BLOG)
+        data = response.json()
+        return data
