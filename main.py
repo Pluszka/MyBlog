b14d825e93294ee6e17ce7c4cec994d76924c0fc
Pliszka <karoliniakkjk@gmail.com>
2022-04-06 21:18:37 +0200

    articles pages

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", articles=database.get_blog() )
+    return render_template("index.html", articles=database.get_blog())
+
+@app.route('/article/<article_id>')
+def article_page(article_id):
+    return render_template("post.html", article=database.get_blog()[int(article_id)-1])
 
 if __name__ == "__main__":
     app.run(debug=True)
